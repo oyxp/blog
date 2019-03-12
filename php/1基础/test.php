37e4d55499daec962b63f81c110c373f6be664b2
oyxp <584119837@qq.com>
2019-03-12 12:51:24 +0800

    目录结构

---
+++ php/1基础/test.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: snail
+ * Date: 2019/3/10
+ * Time: 11:00 PM
+ */
+////正常定义常量
+//const A = 1;//正确
+//define('A1', 2);//正确
+//
+////使用表达式定义
+//for ($i = 0; $i < 3; $i++)
+//{
+//	define('B' . $i, $i);//正确
+//	//const 'B'.$i = $i;//错误
+//}
+//
+////class A
+////{
+////	const STATUS = 10; //正确
+////	//define('D',1); // 错误
+////}
+////
+////
+////echo A, PHP_EOL;//1
+////echo A1, PHP_EOL;//2
+////echo B0, PHP_EOL;//0
+////echo B1, PHP_EOL;//1
+////echo B2, PHP_EOL;//2
+////echo A::STATUS, PHP_EOL;//10
+//
+//
+//if (8 - 6.4 == 1.6)
+//{
+//	echo 'TRUE';
+//}
+//else
+//{
+//	echo 8 - 6.4, PHP_EOL;
+//	echo 'FALSE', PHP_EOL;
+//}
+//
+//$string1 = "I am a string!\n";//双引号定义
+//
+//$string2 = 'I am a string!\n';//单引号定义
+//
+//$heredoc = <<<EOF
+//I am a string!\n
+//I am a string!\n
+//heredoc方式定义的字符串 ${string1}\n;
+//EOF;
+//
+//$newdoc = <<<'EOF'
+//I am a string!\n
+//I am a string!\n
+//newdoc方式定义的字符串 ${string1}\n;
+//EOF;
+//
+//echo $string1;
+//echo $string2;
+//echo $heredoc;
+//echo $newdoc;
+//
+//
+////$a = NULL;
+////$b;//NULL
+////
+////$d = 1;
+////unset($d);
+////
+////var_dump($a);//NULL
+////var_dump($b);//NULL
+////var_dump($c);//NULL
+////var_dump($d);//NULL
+//
+//
+//function f1()
+//{
+//	echo 'f1', PHP_EOL;
+//}
+//
+//class A
+//{
+//	public function f2()
+//	{
+//		echo 'A->f2', PHP_EOL;
+//	}
+//
+//	public static function f3()
+//	{
+//		echo static::class . '::f3' . PHP_EOL;
+//	}
+//}
+//
+//class B extends A
+//{
+//
+//}
+//
+//call_user_func('f1');
+//call_user_func_array('f1', []);
+//
+//$a = new A;
+//call_user_func([$a, 'f2']);
+//call_user_func_array([$a, 'f2'], []);
+//
+//
+//call_user_func([A::class, 'f3']);
+//call_user_func_array([A::class, 'f3'], []);
+//
+//call_user_func([B::class, 'f3']);
+
+//
+//namespace a\b;
+//
+//class Person
+//{
+//	use P;
+//
+//	public function a()
+//	{
+//		echo __CLASS__;
+//	}
+//}
+//
+//trait P
+//{
+//	public function b()
+//	{
+//		echo __TRAIT__;
+//		echo 2 ** 3;
+//	}
+//}
+//
+////(new Person())->b();
+//$dir = '.';
+//echo `ls ${dir}`;
+////var_dump(shell_exec('ls -al'));
+///
+interface Animal {
+	public function eat();
+}
+
+class Cat implements Animal{
+	public function eat(){
+		echo 'cat eat';
+	}
+}
+
+$a = new Cat;
+echo $a instanceof Animal ? 'YES':'NO';//YES
+echo $a instanceof Cat ? 'YES':'NO';//YES
+die;
